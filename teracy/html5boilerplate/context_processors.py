10e3236099310f8fc8acdb7986865e11def4d274
Hoat Le <hoatlevan@gmail.com>
2013-08-19 18:10:25 +0700

    DJHBP-4 | implement context processor

--- teracy/html5boilerplate/context_processors.py
+++ teracy/html5boilerplate/context_processors.py
@@ -1,7 +1,7 @@
 """
 A set of request context processors provied by teracy.html5boilerplate.
 """
-#from django.conf import settings
+from django.conf import settings
 
 
 def page(request):
@@ -17,7 +17,14 @@ def page(request):
 
     page_context = {}
 
-    #TODO set values here
+    if settings.SITE_AUTHOR is not None:
+        page_context['author'] = settings.SITE_AUTHOR
+
+    if settings.SITE_COPYRIGHT is not None:
+        page_context['copyright'] = settings.SITE_COPYRIGHT
+
+    if settings.SITE_GA_ID is not None:
+        page_context['ga_id'] = settings.SITE_GA_ID
 
     context_extras['page'] = page_context
 
