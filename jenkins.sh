a61b94473d2101d5e844a19f267b2b7c1afb4e67
Hoat Le <hoatlevan@gmail.com>
2013-09-13 10:38:05 +0000

    DJHBP-14 | Add pylint, pep8 violations report, unit test report via nosetests: add nosetests.xml for jenkins

--- jenkins.sh
+++ jenkins.sh
@@ -14,7 +14,7 @@ pylint --rcfile .pylintrc -f parseable *.py teracy > pylint_report.txt || exit 0
 
 # test
 export DJANGO_SETTINGS_MODULE=teracy.html5boilerplate.test_settings
-coverage run --branch --source=teracy `which nosetests` --with-xunit
+coverage run --branch --source=teracy `which nosetests` --with-xunit --xunit-file=nosetests.xml
 
 # coverage report
 coverage xml --omit=*/__init__.py,teracy/html5boilerplate/test*
